f0ec381b94f299f1c8a50a3a897d738283518fdc
PcmBego <97907164+PcmBego@users.noreply.github.com>
2022-05-20 17:29:44 +0900

    Delete Project_With directory

--- Project_With/crawler_pkg/__init__.py
+++
@@ -1,6 +0,0 @@
-# 패키지 외부에 공개할 모듈, 변수, 함수, 클래스를 리스트 형태로 지정
-__all__ = ["InstagramCrawler", "crawling_process", "visual_wordcloud_process", "visual_map_process"]
-
-from .insta_crawler import *    # insta_crawler 모듈의 모든 변수, 함수, 클래스를 포함합니다. 
-from .crawling_process import *
-from .visualization import *
--- Project_With/crawler_pkg/visualization/__init__.py
+++
@@ -1,5 +0,0 @@
-# 패키지 외부에 공개할 모듈, 변수, 함수, 클래스를 리스트 형태로 지정
-__all__ = ["visual_wordcloud_process", "visual_map_process"]
-
-from .visualization_wordcloud import *
-from .visualization_map import *
